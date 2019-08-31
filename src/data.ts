96bfd3b6effc397a8fcbfbf6801e50ae82310d75
munshidun <munshidun@gmail.com>
2019-08-31 17:48:15 +0500

    Create data

---
+++ src/data.ts
@@ -0,0 +1,26 @@
+export const Subjects: Object[] = [
+    {
+        id: 1, title: "Abdullah ibn Masud",
+    },
+    {
+        id: 2, title: "Abd"
+    }
+]
+
+export const Definitions: Object[] = [
+    {
+        id: 1, definition: "Abdullah ibn Masud (c.594-c.653) was a companion of the Islamic prophet, Muhammad.",
+    },
+    {
+        id: 2, definition: "Abd - servant, worshipper, slave. Muslims consider themselves servants and worshippers of Allah."
+    }
+]
+
+export const Types: Object[] = [
+    {
+        id: 1, name: "Person",
+    },
+    {
+        id: 2, name: "Term"
+    }
+]
